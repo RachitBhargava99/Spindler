8e09bd31f11fbd8dbf359e686d1c86beec665fb5
RachitBhargava99 <rachitbhargava99@gmail.com>
2019-03-03 17:34:07 -0500

    Results Loading in Database - Working

--- backend/events/routes.py
+++ backend/events/routes.py
@@ -46,7 +46,7 @@ def search_now():
     received_res = result['collection']['items'] ##storing search results until all are accumulated
     num_p_extra = result['collection']['metadata']['total_hits'] // 100 ##number of more pages to load data from
 
-    for i in range(1, num_p_extra + 1):
+    for i in range(2, num_p_extra + 2):
         map_to_send['page'] = i
         query = requests.get(current_app.config['BASE_URL'] + current_app.config['SEARCH_URL'], params=map_to_send)
         result = query.json()
@@ -58,15 +58,19 @@ def search_now():
         if (Result.query.filter_by(nasa_id=each['data'][0]['nasa_id'])).first() is None:
             new_res = Result(
                 name=each['data'][0]['title'],
-                center=each['data'][0]['center'],
+                center=each['data'][0]['center']
+                if each['data'][0].get('center') is not None else "No Center Information Provided",
                 last_updated=datetime.strptime(each['data'][0]['date_created'], "%Y-%m-%dT%H:%M:%SZ"),
-                thumb_img=[x['href'] for x in each['links']
-                           if x.get('render') is not None and x['render'] == "image"][0],
-                description=each['data'][0]['description'],
+                thumb_img=([x['href'] for x in each['links']
+                           if x.get('render') is not None and x['render'] == "image"][0])
+                if each.get('links') is not None else "",
+                description=((each['data'][0]['description'])
+                if len(each['data'][0]['description']) <= 16383 else each['data'][0]['description'][:16379] + "...")
+                if each['data'][0].get('description') is not None else "",
                 nasa_id=each['data'][0]['nasa_id']
             )
             db.session.add(new_res)
             num_res += 1
     db.session.commit()
 
-    return json.dumps({'data': received_res, 'num_res': num_res})
+    return json.dumps({'data': received_res, 'num_res': num_res, 'num_ret': result['collection']['metadata']['total_hits']})
